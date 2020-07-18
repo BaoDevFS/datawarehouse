ac7480d7749ed1f4bff0c22be448fbe12bd38543
hienpham761311 <mh.pham761311@gmail.com>
2020-07-18 16:01:58 +0700

    recommit 18/7/20

--- src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-	public static void main(String[] args) {
-		String url = "jdbc:mysql://localhost:3306/wasehouse?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-
-	}
-
-}
