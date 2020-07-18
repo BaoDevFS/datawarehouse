b516c4ffc57e2504a8dc7b4bb106760ae713c183
hienpham761311 <mh.pham761311@gmail.com>
2020-07-18 14:56:54 +0700

    reconvert data from staging to wasehouse

---
+++ src/dao/Wasehouse.java
@@ -0,0 +1,97 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import model.DataHandle;
+import model.SinhVien;
+
+public class Wasehouse {
+	
+	//getAll data wasehouse
+	public static ArrayList<SinhVien> getAllSinhVien(){
+		Connection con = ConnectDB.getConnection();
+		//get data wasehouse
+				ArrayList<SinhVien> wasehouses = new ArrayList<SinhVien>();
+				String sqlWasehouse = "select * from wsinhvien";
+				Statement statement;
+				
+				try {
+					
+					statement = con.createStatement();
+					ResultSet rs = statement.executeQuery(sqlWasehouse);
+					while(rs.next()){
+						wasehouses.add(new SinhVien(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10),rs.getString(11),rs.getString(12),rs.getString(13)));
+						
+					}
+					
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				return wasehouses;
+	}
+	//cap nhat du lieu dua vao id
+	public static void updateData(String id) {
+		Connection con = ConnectDB.getConnection();
+		//update to wasehouse
+		String sql = "update wSinhVien set expired = ? where id = ?";
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(2, id);
+			//khong gioi han thoi gian het han
+			ps.setDate(1, new Date(1-1900, 11, 31));
+			ps.execute();
+			
+		} catch (SQLException e) {
+			
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	// insert data wasehouse (1 row)
+	public static void insertData(SinhVien st) {
+		Connection con = ConnectDB.getConnection();
+		//insert to wasehouse
+		String sql = "insert into wSinhVien values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement(sql);
+			
+			ps.setString(1, DataHandle.setId());
+			ps.setString(2, st.stt);
+			ps.setString(3, st.mssv);
+			ps.setString(4, st.lastName);
+			ps.setString(5, st.firstName);
+			ps.setString(6, st.dayBorn);
+			ps.setString(7, st.classId);
+			ps.setString(8, st.className);
+			ps.setString(9, st.phoneNumber);
+			ps.setString(10, st.email);
+			ps.setString(11, st.address);
+			ps.setString(12, st.note);
+			//khong gioi han thoi gian het han
+			ps.setDate(13, new Date(9999-1900, 11, 31));
+			ps.execute();
+			
+		} catch (SQLException e) {
+			
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+//		SinhVien st = new SinhVien("1", "mssv", "lastName", "firstName", "dayBorn", "classId", "className", "phoneNumber", "email", "address", "note");
+//		Wasehouse.insertData(st);
+//		System.out.println(Wasehouse.getAllSinhVien().toString());
+//		updateData("06cee6fd-2ef5-44bb-9200-defb3b39a277");
+//		System.out.println(Wasehouse.getAllSinhVien().toString());
+	}
+
+}
--- src/model/DataHandle.java
+++ src/model/DataHandle.java
@@ -1,14 +1,14 @@
-package wasehouse;
+package model;
 
 import java.util.Date;
 import java.util.StringTokenizer;
+import java.util.UUID;
 import java.text.SimpleDateFormat;
 
 public class DataHandle {
 	
 	
-	public static Date getDate() {
-		Date now = new Date();
+	public static Date getDate(Date now) {
         String pattern = "yyyy-MM-dd";
         SimpleDateFormat formatter = new SimpleDateFormat(pattern);
         String mysqlDateString = formatter.format(now);
@@ -17,5 +17,7 @@ public class DataHandle {
         Date d = new java.sql.Date(Integer.parseInt(st.nextToken())-1900, Integer.parseInt(st.nextToken())-1,Integer.parseInt(st.nextToken()));
        return d;
 	}
-
+	public static String setId() {
+		return UUID.randomUUID().toString();
+	}
 }
