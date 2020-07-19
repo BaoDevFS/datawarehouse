f4a5f54aaaba3aecef8e920df1f7870d6d9d87e9
17130010@st.hcmuaf.edu.vn <Baonguyen1999>
2020-07-19 15:21:51 +0700

    edt package

--- src/mainprocessv/ProcessSinhVien.java
+++ src/mainprocessv/ProcessSinhVien.java
@@ -1,17 +1,18 @@
-package mainProcess;
+package mainprocessv;
 
 import getdata.CollectData;
-import part2.TranfertoStaging;
 
 public class ProcessSinhVien {
 	public ProcessSinhVien() {
-		
+
 	}
+
 	public void runConfig(int id) {
 		CollectData collectData = new CollectData();
-		TranfertoStaging tranfertoStaging = new TranfertoStaging();
+//		TranfertoStaging tranfertoStaging = new TranfertoStaging();
 		collectData.startTask(id);
 	}
+
 	public static void main(String[] args) {
 		ProcessSinhVien pr = new ProcessSinhVien();
 		pr.runConfig(Integer.parseInt(args[0]));
